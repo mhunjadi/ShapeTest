182f81f92b5089f3bb9422fd1ad0cdf8170d5201
mhunjadi <marinhunjadi@gmail.com>
2024-04-20 23:44:49 +0200

    Optimization

--- testfile.py
+++ testfile.py
@@ -1,5 +1,6 @@
 
 import math
+from itertools import combinations
 
 class Shape:
     def __init__(self, points):
@@ -35,14 +36,42 @@ class Rectangle(Shape):
         return math.sqrt(sum((coord1 - coord2) ** 2 for coord1, coord2 in zip(point1, point2)))
 
 class Cuboid(Shape):
-    def is_cuboid(self, n):
-        vectors = [[self.points[j][i] - self.points[0][i] for i in range(len(self.points[0]))] for j in range(1, n)]
-        for i in range(len(vectors)):
-            for j in range(i + 1, len(vectors)):
-                if sum(vectors[i][k] * vectors[j][k] for k in range(len(vectors[i]))) != 0:
-                    return False
-        return True
-    
+    def is_cuboid(self):
+        A, B, C = self.points[0:3]
+        AB = self.distance(A, B)
+        BC = self.distance(B, C)
+        AC = self.distance(A, C)
+        is_rectangle = (math.sqrt(AB ** 2 + BC ** 2) == AC) or (math.sqrt(AB ** 2 + AC ** 2) == BC) or (math.sqrt(AC ** 2 + BC ** 2) == AB)
+        if is_rectangle:
+            P = self.find_fourth_edge(A, B, C)
+            if P:
+                P = (P[0], P[1], A[2])
+            D = self.points[-1]
+            AD = (A[0], A[1], D[2])
+            BD = (B[0], B[1], D[2])
+            CD = (C[0], C[1], D[2])
+            PD = (P[0], P[1], D[2])
+            if D == AD or D == BD or D == CD or (P and D == PD):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    @staticmethod    
+    def find_fourth_edge(point1, point2, point3):
+        # Izracun vektora iz prvog vrha pravokutnika prema druga dva
+        vector1 = (point2[0] - point1[0], point2[1] - point1[1])
+        vector2 = (point3[0] - point1[0], point3[1] - point1[1])
+
+        # Provjera da li su tri tocke vrhovi pravokutnika
+        if vector1[0] * vector2[0] + vector1[1] * vector2[1] != 0:
+            return False
+
+        # Izracun tocke cetvrtog vrha pravokutnika
+        point4 = (point1[0]+ vector1[0] + vector2[0], point1[1] + vector1[1] + vector2[1])
+        return point4        
+        
     def is_inside(self, point):
         A, B, C, D = self.points
         min_bounds = [min(A[i], B[i], C[i], D[i]) for i in range(len(A))]
@@ -53,7 +82,7 @@ class Cuboid(Shape):
         A, B, C, D = self.points
         length = self.distance(A, B)
         width = self.distance(A, C)
-        height = self.distance(A, D)
+        height = self.distance(A, (A[0], A[1], D[2]))
 
         return math.sqrt(length ** 2 + width ** 2 + height ** 2)
 
@@ -73,18 +102,14 @@ def main():
         elif len(coordinates[0]) == len(coordinates[1]) == len(coordinates[2]) == 3:
             shape = Cuboid(coordinates[0:4])
         else:
-            try:
-                shape = Cuboid(coordinates[0:5])
-            except:
-                print("Invalid file format.")
-                return
+            raise ValueError("Undefined shape or bad format.")
 
         if isinstance(shape, Rectangle):
             if not shape.is_rectangle():
                 print("Given points do not form a rectangle.")
                 return
         elif isinstance(shape,Cuboid):
-            if not shape.is_cuboid(4):
+            if not shape.is_cuboid():
                 print("Given points do not form a cuboid.")
                 return
 
